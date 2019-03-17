4274ea57a190dbec3d71d6f4a2c7f0157fce4a96
Tobias Fankhänel <8aviav@gmail.com>
2019-03-17 14:58:12 +0100

    Update Dialyxir and Ex_Doc

--- mix.exs
+++ mix.exs
@@ -35,8 +35,8 @@ defmodule Org.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
-      {:dialyxir, "~> 0.5.1"}
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.4", only: :dev, runtime: false}
     ]
   end
 end
